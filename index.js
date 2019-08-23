714abf4ba5095dd38bd234fd5af2f7864ffc82f8
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-08-22 22:24:10 -0500

    segundo commit

--- index.js
+++ index.js
@@ -10,9 +10,30 @@ server.listen(3000,function(){
 });*/
 
 /*servidor con express*/
+
+/**rutass */
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+/*middleware*/
+function logger(req,res,next){
+    console.log("ruta recibida"+req.protocol+" "+req.get("host")+req.originalUrl);
+    next();
+
+}
+
 app.use(express.json());
+app.use(logger);
+app.use(morgan("dev"));
+/**rutass */
+/*app.all("/user",function(req,res,next){
+    console.log("por aqui paso");
+    next();
+    
+});*/
+app.get("/",function(req,res){
+    res.send("hola");
+;});
 app.get("/user",function(req,res){
     res.json({
         username:"cameron",
@@ -24,8 +45,9 @@ app.post("/user/:id",function(req,res){
    console.log(req.params);
    res.send("peticion post recibida");
 });
-app.put("/contact",function(req,res){
-    res.send("actualizacion request");
+app.put("/user/:id",function(req,res){
+    res.send("el usuaeio "+req.params.id+"fue actualizado");
+    console.log(req.body);
  });
  app.delete("/user/:id",function(req,res){
        res.send("el usuaeio "+req.params.id+"fue eliminado");
@@ -34,3 +56,4 @@ app.put("/contact",function(req,res){
 app.listen(5000,function(){
     console.log("hola mundo con express");
 });
+/***************** */
